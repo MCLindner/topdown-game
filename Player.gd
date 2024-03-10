085dad0e0239b9dd146b2a82479550cbb4f2cd26
Michael <Lindner.MCL@proton.me>
2024-03-09 21:37:52 -0800

    first commit

---
+++ Player.gd
@@ -0,0 +1,87 @@
+extends CharacterBody2D
+
+signal hit
+signal game_over
+
+@export var speed = 100 # How fast the player will move (pixels/sec).
+@export var player_health = 3 # Player health duh
+var screen_size # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1	
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		
+	move_and_slide()
+	handle_collisions()
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		# $AnimatedSprite2D.flip_v = velocity.y > 0 
+
+## Collision detection v2.
+func handle_collisions():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		print("I collided with ", collision.get_collider().name)
+		if collision.get_collider().name.begins_with("@RigidBody2D") == true:
+			hit.emit() # HUD needs to receive this signal
+			player_health -= 1
+			$CollisionShape2D.set_deferred("disabled", true)
+			$InvulnTimer.start()
+
+			# use $CollisionShape2D.set_deferred("disabled", true) perhaps
+			
+			# this needs to check for player health
+			if player_health == 0:
+				game_over.emit()
+				hide() # Player disappears after being hit.
+				# Must be deferred as we can't change physics properties on a physics callback.
+				# Maybe disable the hitbox for a half a second for i frames
+				$CollisionShape2D.set_deferred("disabled", true)
+		
+
+## Collision detection.
+#func _on_body_entered(body):
+	#hide() # Player disappears after being hit.
+	#hit.emit()
+	## Must be deferred as we can't change physics properties on a physics callback.
+	#$CollisionShape2D.set_deferred("disabled", true)
+	
+
+# Reset the player upon starting 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+func _on_invuln_timer_timeout():
+	$CollisionShape2D.set_deferred("disabled", false)
