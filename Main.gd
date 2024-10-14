4f57a8f8f046276ed9d6a3ee142f7c8bfecdde41
Michael <Lindner.MCL@proton.me>
2024-10-13 23:02:15 -0700

    made enemies go toward player

--- Main.gd
+++ Main.gd
@@ -41,7 +41,7 @@ func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
 	
 	# Choose a random location on Path2D.
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	var mob_spawn_location = get_node("Player/MobPath/MobSpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 	
 	# Set the mob's direction perpendicular to the path direction.
@@ -49,11 +49,13 @@ func _on_mob_timer_timeout():
 
 	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
+	
+	
 
 	# Add some randomness to the direction.
 	direction += randf_range(-PI / 4, PI / 4)
 	mob.rotation = direction
-
+	
 	# Choose the velocity for the mob.
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
@@ -72,4 +74,3 @@ func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
 	
-
--- Mob.gd
+++ Mob.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
-
+@export var speed = 3
+@export var accel = 2
+@onready var player = get_parent().get_node("Player")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
@@ -9,7 +11,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var player_pos = player.get_position()
+	var target_pos = (player_pos - position).normalized()
+	linear_velocity = linear_velocity.lerp(target_pos * speed, accel * delta)
+
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
